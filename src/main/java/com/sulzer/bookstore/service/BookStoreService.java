3540b1a8bf43601c65b8b11bed9ac0da749f57ba
maidacencoanastasia <maidacenco100@gmail.com>
2022-01-11 12:21:37 +0200

    Initial commit

---
+++ src/main/java/com/sulzer/bookstore/service/BookStoreService.java
@@ -0,0 +1,31 @@
+package com.sulzer.bookstore.service;
+
+import com.sulzer.bookstore.constants.Category;
+import com.sulzer.bookstore.service.dto.BookDto;
+import com.sulzer.bookstore.service.dto.SellDto;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public interface BookStoreService {
+    void addNewBook(BookDto bookDto);
+
+    void addBook(Long id, int quantityToAdd);
+
+    BookDto getBookById(Long id);
+
+    List<BookDto> getAllBooks();
+
+    int getNumberOfBooksById(Long id);
+
+    void updateBook(Long id, BookDto bookDto);
+
+    void sellBook(Long id);
+
+    void sellBooks(List<SellDto> sellDtos);
+
+    List<BookDto> getBookByCategoryKeyWord(String keyword, Category category);
+
+    int getNumberOfBooksSoldByCategoryAndKeyword(String keyword, Category category);
+
+}
